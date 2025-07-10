9ea0bf4f2b62b5f57950878dd32c07f7df90913c
sx4im <sx4imm@gmail.com>
2025-07-10 16:50:00 +0500

    solve 27,28

---
+++ Easy/028.js
@@ -0,0 +1,18 @@
+// 28. Find the Index of the First Occurrence in a String
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (needle === "") return 0;
+
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
