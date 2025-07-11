41ccbe21d9ddd908dd75b638b7bd2147f6257131
sx4im <sx4imm@gmail.com>
2025-07-11 12:09:11 +0500

    solve 036,038

--- Medium/038.js
+++ Medium/038.js
@@ -1,1 +1,23 @@
 // 38. Count and Say
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    if (n === 1) return "1";
+
+    let prev = countAndSay(n - 1);
+    let result = '';
+    let count = 1;
+
+    for (let i = 1; i <= prev.length; i++) {
+        if (prev[i] === prev[i - 1]) {
+            count++;
+        } else {
+            result += count.toString() + prev[i - 1];
+            count = 1;
+        }
+    }
+
+    return result;
+};
