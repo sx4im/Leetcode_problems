1ee354a64ad8a83ef26bb50c68794812f3869254
sx4im <sx4imm@gmail.com>
2025-07-11 12:05:26 +0500

    solve 022,024

--- Medium/024.js
+++ Medium/024.js
@@ -1,1 +1,23 @@
 // 24. Swap Nodes in Pairs
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (!head || !head.next) return head;
+
+    let first = head;
+    let second = head.next;
+
+    first.next = swapPairs(second.next);
+    second.next = first;
+
+    return second;
+};
