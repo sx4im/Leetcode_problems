ccb767cc18b3d1df3562974740d82b9ef8e578b7
sx4im <sx4imm@gmail.com>
2025-07-11 12:15:18 +0500

    solve 049,050

--- Medium/049.js
+++ Medium/049.js
@@ -1,1 +1,18 @@
 // 49. Group Anagrams
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = new Map();
+
+    for (let str of strs) {
+        const sorted = str.split('').sort().join('');
+        if (!map.has(sorted)) {
+            map.set(sorted, []);
+        }
+        map.get(sorted).push(str);
+    }
+
+    return Array.from(map.values());
+};
