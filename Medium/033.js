0e21d7ac8c0e6e677a85b3101916512ea9462e9e
sx4im <sx4imm@gmail.com>
2025-07-11 12:08:05 +0500

    solve 033,034

--- Medium/033.js
+++ Medium/033.js
@@ -1,1 +1,27 @@
 // 33. Search in Rotated Sorted Array
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        // Left part is sorted
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        } else {
+            // Right part is sorted
+            if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+            else right = mid - 1;
+        }
+    }
+
+    return -1;
+};
--- Medium/034.js
+++ Medium/034.js
@@ -1,1 +1,25 @@
 // 34. Find First and Last Position of Element in Sorted Array
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    const findIndex = (isFirst) => {
+        let left = 0, right = nums.length - 1, index = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+            if (nums[mid] === target) {
+                index = mid;
+                if (isFirst) right = mid - 1;
+                else left = mid + 1;
+            } else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+
+        return index;
+    };
+
+    return [findIndex(true), findIndex(false)];
+};
