5e4f55cb0b71b9c7b4e0d2b64b474ed744d914c7
sx4im <sx4imm@gmail.com>
2025-07-11 12:14:49 +0500

    solve 048

--- Medium/048.js
+++ Medium/048.js
@@ -1,1 +1,18 @@
 // 48. Rotate Image
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const n = matrix.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let row of matrix) {
+        row.reverse();
+    }
+};
