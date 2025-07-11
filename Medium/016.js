90198781fc2e5cb2982cae3b1d11d1c5b112a712
sx4im <sx4imm@gmail.com>
2025-07-11 12:02:42 +0500

    solve 016,017

--- Medium/016.js
+++ Medium/016.js
@@ -1,1 +1,26 @@
 // 16. 3Sum Closest
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b);
+    let closest = nums[0] + nums[1] + nums[2];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1, right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                closest = sum;
+            }
+
+            if (sum < target) left++;
+            else right--;
+        }
+    }
+
+    return closest;
+};
--- Medium/017.js
+++ Medium/017.js
@@ -1,1 +1,29 @@
 // 17. Letter Combinations of a Phone Number
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    if (!digits) return [];
+
+    const map = {
+        "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+        "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+    };
+
+    const result = [];
+
+    const backtrack = (i, path) => {
+        if (i === digits.length) {
+            result.push(path);
+            return;
+        }
+
+        for (let char of map[digits[i]]) {
+            backtrack(i + 1, path + char);
+        }
+    };
+
+    backtrack(0, "");
+    return result;
+};
