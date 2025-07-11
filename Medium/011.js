7c7fe439cf217d96dabed37927a14189a904b761
sx4im <sx4imm@gmail.com>
2025-07-11 12:00:57 +0500

    solve 011,012

--- Medium/011.js
+++ Medium/011.js
@@ -1,1 +1,20 @@
 // 11. Container With Most Water
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0, right = height.length - 1;
+    let maxArea = 0;
+
+    while (left < right) {
+        const h = Math.min(height[left], height[right]);
+        const w = right - left;
+        maxArea = Math.max(maxArea, h * w);
+
+        if (height[left] < height[right]) left++;
+        else right--;
+    }
+
+    return maxArea;
+};
--- Medium/012.js
+++ Medium/012.js
@@ -1,1 +1,20 @@
 // 12. Integer to Roman
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const syms = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+    let res = "";
+
+    for (let i = 0; i < val.length; i++) {
+        while (num >= val[i]) {
+            res += syms[i];
+            num -= val[i];
+        }
+    }
+
+    return res;
+};
