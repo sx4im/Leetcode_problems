f8adf7751b7a263457d51eecb474ac2bccc77648
sx4im <sx4imm@gmail.com>
2025-07-11 12:11:58 +0500

    solve 045

--- Medium/045.js
+++ Medium/045.js
@@ -1,1 +1,18 @@
 // 45. Jump Game II
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    let jumps = 0, end = 0, farthest = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        farthest = Math.max(farthest, i + nums[i]);
+        if (i === end) {
+            jumps++;
+            end = farthest;
+        }
+    }
+
+    return jumps;
+};
