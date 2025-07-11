f72c3fb229f7dd5fc627c41a5d7f50ca4d99d1f5
sx4im <sx4imm@gmail.com>
2025-07-11 12:07:07 +0500

    solve 031

--- Medium/031.js
+++ Medium/031.js
@@ -1,1 +1,25 @@
 // 31. Next Permutation
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place.
+ */
+var nextPermutation = function(nums) {
+    let i = nums.length - 2;
+
+    // Find first decreasing element from the end
+    while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+
+    if (i >= 0) {
+        let j = nums.length - 1;
+        while (nums[j] <= nums[i]) j--;
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    // Reverse the suffix
+    let left = i + 1, right = nums.length - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+};
