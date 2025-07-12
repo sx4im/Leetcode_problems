163e2dff105c865f67c7a9168725efa84e5cd276
sx4im <sx4imm@gmail.com>
2025-07-12 10:51:45 +0500

    solve 032

--- Hard/032.js
+++ Hard/032.js
@@ -1,1 +1,24 @@
 // 32. Longest Valid Parentheses
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function(s) {
+    let stack = [-1];
+    let maxLength = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            stack.push(i);
+        } else {
+            stack.pop();
+            if (stack.length === 0) {
+                stack.push(i);
+            } else {
+                maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
+            }
+        }
+    }
+
+    return maxLength;
+};
