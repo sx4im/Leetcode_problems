4b6cd6d9056be51ce64be4f6337b1ff468e8a8bb
sx4im <sx4imm@gmail.com>
2025-07-13 11:10:10 +0500

    solve next problems

--- Medium/082.js
+++ Medium/082.js
@@ -1,1 +1,30 @@
 // 82. Remove Duplicates from Sorted List II
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    const dummy = new ListNode(0, head);
+    let prev = dummy;
+
+    while (head) {
+        if (head.next && head.val === head.next.val) {
+            while (head.next && head.val === head.next.val) {
+                head = head.next;
+            }
+            prev.next = head.next;
+        } else {
+            prev = prev.next;
+        }
+        head = head.next;
+    }
+
+    return dummy.next;
+};
