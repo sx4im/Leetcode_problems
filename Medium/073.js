f22258443d04267676eb0972a5a129427f9f99ac
sx4im <sx4imm@gmail.com>
2025-07-13 11:06:49 +0500

    solve next problems

--- Medium/073.js
+++ Medium/073.js
@@ -1,1 +1,35 @@
 // 73. Set Matrix Zeroes
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let firstRowZero = false, firstColZero = false;
+    const m = matrix.length, n = matrix[0].length;
+
+    // First row and column flags
+    for (let i = 0; i < m; i++) if (matrix[i][0] === 0) firstColZero = true;
+    for (let j = 0; j < n; j++) if (matrix[0][j] === 0) firstRowZero = true;
+
+    // Mark zeros in first row and col
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+
+    // Zero out marked cells
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (firstRowZero) for (let j = 0; j < n; j++) matrix[0][j] = 0;
+    if (firstColZero) for (let i = 0; i < m; i++) matrix[i][0] = 0;
+};
--- Medium/074.js
+++ Medium/074.js
@@ -1,1 +1,21 @@
 // 74. Search a 2D Matrix
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length, n = matrix[0].length;
+    let left = 0, right = m * n - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let val = matrix[Math.floor(mid / n)][mid % n];
+
+        if (val === target) return true;
+        else if (val < target) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return false;
+};
