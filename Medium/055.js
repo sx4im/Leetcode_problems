b6fc899d5d5853b25e5f3b2f7fa4815f87070a69
sx4im <sx4imm@gmail.com>
2025-07-13 11:01:46 +0500

    solve next problems

--- Medium/055.js
+++ Medium/055.js
@@ -1,1 +1,13 @@
 // 55. Jump Game
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let farthest = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i > farthest) return false;
+        farthest = Math.max(farthest, i + nums[i]);
+    }
+    return true;
+};
--- Medium/056.js
+++ Medium/056.js
@@ -1,1 +1,24 @@
 // 56. Merge Intervals
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (!intervals.length) return [];
+
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [intervals[0]];
+
+    for (let i = 1; i < intervals.length; i++) {
+        const last = merged[merged.length - 1];
+        const curr = intervals[i];
+
+        if (curr[0] <= last[1]) {
+            last[1] = Math.max(last[1], curr[1]);
+        } else {
+            merged.push(curr);
+        }
+    }
+
+    return merged;
+};
