984ad8a8c0944f00e6e828d6a3e340715706c3cd
sx4im <sx4imm@gmail.com>
2025-07-13 11:12:47 +0500

    solve next problems

--- Medium/092.js
+++ Medium/092.js
@@ -1,1 +1,32 @@
-// 92. Reverse Linked List II
+// 92. Reverse Linked List IIq2w3e4yuiikjh<xm
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    const dummy = new ListNode(0, head);
+    let prev = dummy;
+
+    for (let i = 1; i < left; i++) {
+        prev = prev.next;
+    }
+
+    let current = prev.next;
+    for (let i = 0; i < right - left; i++) {
+        const temp = current.next;
+        current.next = temp.next;
+        temp.next = prev.next;
+        prev.next = temp;
+    }
+
+    return dummy.next;
+};
--- Medium/093.js
+++ Medium/093.js
@@ -1,1 +1,30 @@
 // 93. Restore IP Addresses
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var restoreIpAddresses = function(s) {
+    const result = [];
+
+    function backtrack(start, path) {
+        if (path.length === 4 && start === s.length) {
+            result.push(path.join('.'));
+            return;
+        }
+
+        if (path.length >= 4) return;
+
+        for (let len = 1; len <= 3; len++) {
+            if (start + len > s.length) break;
+            const part = s.substring(start, start + len);
+            if ((part.length > 1 && part[0] === '0') || parseInt(part) > 255) continue;
+
+            path.push(part);
+            backtrack(start + len, path);
+            path.pop();
+        }
+    }
+
+    backtrack(0, []);
+    return result;
+};
