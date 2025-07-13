012ad0103d622655a79d198447737a88a2a6f9d7
sx4im <sx4imm@gmail.com>
2025-07-13 11:00:56 +0500

    solve next problems

--- Medium/053.js
+++ Medium/053.js
@@ -1,1 +1,13 @@
 // 53. Maximum Subarray
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum = nums[0], currSum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        currSum = Math.max(nums[i], currSum + nums[i]);
+        maxSum = Math.max(maxSum, currSum);
+    }
+    return maxSum;
+};
