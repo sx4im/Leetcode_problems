195139dd19572a8df92a6a3c57bb95090a27f399
sx4im <sx4imm@gmail.com>
2025-07-13 11:15:16 +0500

    solve next problems

--- Medium/097.js
+++ Medium/097.js
@@ -1,1 +1,23 @@
 // 97. Interleaving String
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function(s1, s2, s3) {
+    const m = s1.length, n = s2.length;
+    if (m + n !== s3.length) return false;
+
+    const dp = Array(m + 1).fill(null).map(() => Array(n + 1).fill(false));
+    dp[0][0] = true;
+
+    for (let i = 0; i <= m; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (i > 0) dp[i][j] |= (dp[i - 1][j] && s1[i - 1] === s3[i + j - 1]);
+            if (j > 0) dp[i][j] |= (dp[i][j - 1] && s2[j - 1] === s3[i + j - 1]);
+        }
+    }
+
+    return dp[m][n];
+};
--- Medium/099.js
+++ Medium/099.js
@@ -1,1 +1,35 @@
 // 99. Recover Binary Search Tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+    let first = null, second = null, prev = new TreeNode(-Infinity);
+
+    function inorder(node) {
+        if (!node) return;
+
+        inorder(node.left);
+
+        if (node.val < prev.val) {
+            if (!first) first = prev;
+            second = node;
+        }
+        prev = node;
+
+        inorder(node.right);
+    }
+
+    inorder(root);
+
+    // Swap values
+    [first.val, second.val] = [second.val, first.val];
+};
