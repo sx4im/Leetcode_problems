72bce72f414fca82195737dc9aa81b69c664c526
sx4im <sx4imm@gmail.com>
2025-07-13 11:03:57 +0500

    solve next problems

--- Medium/063.js
+++ Medium/063.js
@@ -1,1 +1,23 @@
 // 63. Unique Paths II
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    const m = obstacleGrid.length, n = obstacleGrid[0].length;
+    const dp = Array(m).fill().map(() => Array(n).fill(0));
+
+    // First cell
+    dp[0][0] = obstacleGrid[0][0] === 0 ? 1 : 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (obstacleGrid[i][j] === 1 || (i === 0 && j === 0)) continue;
+
+            if (i > 0) dp[i][j] += dp[i - 1][j];
+            if (j > 0) dp[i][j] += dp[i][j - 1];
+        }
+    }
+
+    return dp[m - 1][n - 1];
+};
