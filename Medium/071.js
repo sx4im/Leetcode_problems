b5d99ec4ade7958672a9a33efb0621632f3d3c85
sx4im <sx4imm@gmail.com>
2025-07-13 11:06:04 +0500

    solve next problems

--- Medium/071.js
+++ Medium/071.js
@@ -1,1 +1,17 @@
 // 71. Simplify Path
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function(path) {
+    const parts = path.split('/');
+    const stack = [];
+
+    for (const part of parts) {
+        if (part === '' || part === '.') continue;
+        if (part === '..') stack.pop();
+        else stack.push(part);
+    }
+
+    return '/' + stack.join('/');
+};
