b25f86e421be06bb3a29f3c1cbb27719a954c501
sx4im <sx4imm@gmail.com>
2025-07-13 11:10:59 +0500

    solve next problems

--- Medium/086.js
+++ Medium/086.js
@@ -1,1 +1,33 @@
 // 86. Partition List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    let beforeHead = new ListNode(0), before = beforeHead;
+    let afterHead = new ListNode(0), after = afterHead;
+
+    while (head) {
+        if (head.val < x) {
+            before.next = head;
+            before = before.next;
+        } else {
+            after.next = head;
+            after = after.next;
+        }
+        head = head.next;
+    }
+
+    after.next = null;
+    before.next = afterHead.next;
+
+    return beforeHead.next;
+};
--- Medium/089.js
+++ Medium/089.js
@@ -1,1 +1,17 @@
 // 89. Gray Code
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+    const result = [0];
+
+    for (let i = 0; i < n; i++) {
+        const size = result.length;
+        for (let j = size - 1; j >= 0; j--) {
+            result.push(result[j] | (1 << i));
+        }
+    }
+
+    return result;
+};
