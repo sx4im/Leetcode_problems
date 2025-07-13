9cfa80353ed17d932bcbd4b2ddaf7bee0c24031b
sx4im <sx4imm@gmail.com>
2025-07-13 11:09:24 +0500

    solve next problems

--- Medium/079.js
+++ Medium/079.js
@@ -1,1 +1,33 @@
 // 79. Word Search
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+    const m = board.length, n = board[0].length;
+
+    function dfs(i, j, index) {
+        if (index === word.length) return true;
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] !== word[index]) return false;
+
+        const temp = board[i][j];
+        board[i][j] = '#';
+
+        const found = dfs(i+1, j, index+1) ||
+                      dfs(i-1, j, index+1) ||
+                      dfs(i, j+1, index+1) ||
+                      dfs(i, j-1, index+1);
+
+        board[i][j] = temp;
+        return found;
+    }
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (dfs(i, j, 0)) return true;
+        }
+    }
+
+    return false;
+};
--- Medium/080.js
+++ Medium/080.js
@@ -1,1 +1,14 @@
 // 80. Remove Duplicates from Sorted Array II
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0;
+    for (let n of nums) {
+        if (i < 2 || n !== nums[i - 2]) {
+            nums[i++] = n;
+        }
+    }
+    return i;
+};
