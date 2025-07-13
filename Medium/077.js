3a3e4e8335ea56e613e2084cbb9efaa8e02b7668
sx4im <sx4imm@gmail.com>
2025-07-13 11:08:06 +0500

    solve next problems

--- Medium/077.js
+++ Medium/077.js
@@ -1,1 +1,24 @@
 // 77. Combinations
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const result = [];
+    
+    function backtrack(start, path) {
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+        for (let i = start; i <= n; i++) {
+            path.push(i);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(1, []);
+    return result;
+};
--- Medium/078.js
+++ Medium/078.js
@@ -1,1 +1,20 @@
 // 78. Subsets
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    const result = [];
+
+    function backtrack(start, path) {
+        result.push([...path]);
+        for (let i = start; i < nums.length; i++) {
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(0, []);
+    return result;
+};
