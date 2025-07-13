838facb9855a54a7b5fe9cd5c75847f5df9a4100
sx4im <sx4imm@gmail.com>
2025-07-13 11:02:31 +0500

    solve next problems

--- Medium/057.js
+++ Medium/057.js
@@ -1,1 +1,30 @@
 // 57. Insert Interval
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    const result = [];
+    let i = 0;
+
+    // Add all intervals before newInterval
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        result.push(intervals[i++]);
+    }
+
+    // Merge overlapping intervals
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    result.push(newInterval);
+
+    // Add remaining intervals
+    while (i < intervals.length) {
+        result.push(intervals[i++]);
+    }
+
+    return result;
+};
--- Medium/059.js
+++ Medium/059.js
@@ -1,1 +1,23 @@
 // 59. Spiral Matrix II
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function(n) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+    let top = 0, bottom = n - 1, left = 0, right = n - 1;
+    let num = 1;
+
+    while (top <= bottom && left <= right) {
+        for (let i = left; i <= right; i++) matrix[top][i] = num++;
+        top++;
+        for (let i = top; i <= bottom; i++) matrix[i][right] = num++;
+        right--;
+        for (let i = right; i >= left; i--) matrix[bottom][i] = num++;
+        bottom--;
+        for (let i = bottom; i >= top; i--) matrix[i][left] = num++;
+        left++;
+    }
+
+    return matrix;
+};
