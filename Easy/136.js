b383aa6f98182d9dbdf8658d76b165df7234d6e7
sx4im <sx4imm@gmail.com>
2025-07-14 14:23:08 +0500

    solve next problems

---
+++ Easy/136.js
@@ -0,0 +1,12 @@
+// 136. Single Number
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let result = 0;
+    for (let num of nums) {
+        result ^= num;
+    }
+    return result;
+};
