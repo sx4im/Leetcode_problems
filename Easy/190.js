398fb82178842150d0ba304c877aa41895f80a02
sx4im <sx4imm@gmail.com>
2025-07-14 14:33:40 +0500

    solve next problems

---
+++ Easy/190.js
@@ -0,0 +1,14 @@
+// 190. Reverse Bits
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let result = 0;
+    for (let i = 0; i < 32; i++) {
+        result <<= 1;
+        result |= (n & 1);
+        n >>>= 1;
+    }
+    return result >>> 0; // force unsigned
+};
