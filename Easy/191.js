0185ed3d90f6266a6ecb28d64c89596519de4b72
sx4im <sx4imm@gmail.com>
2025-07-14 14:34:17 +0500

    solve next problems

---
+++ Easy/191.js
@@ -0,0 +1,13 @@
+// 191. Number of 1 Bits
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    while (n !== 0) {
+        count += n & 1;
+        n >>>= 1;
+    }
+    return count;
+};
