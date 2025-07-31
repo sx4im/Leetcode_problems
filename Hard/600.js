a04b2c073bc902eb53b6f44c544e185818e290c6
sx4im <msaimshafique4664@gmail.com>
2025-07-31 17:40:24 +0500

    solve next problem 600

---
+++ Hard/600.js
@@ -0,0 +1,31 @@
+// 600. Non-negative Integers without Consecutive Ones
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findIntegers = function(n) {
+  const binary = n.toString(2);
+  const dp = new Array(binary.length + 1).fill(0);
+  let result = 0;
+
+  dp[0] = 1;
+  dp[1] = 2;
+
+  for (let i = 2; i <= binary.length; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  for (let i = 0, previous = 0; i < binary.length; i++) {
+    if (binary[i] === '1') {
+      result += dp[binary.length - i - 1];
+      if (previous === 1) {
+        return result;
+      }
+      previous = 1;
+    } else {
+      previous = 0;
+    }
+  }
+
+  return result + 1;
+};
