87200e8370dc3f35326c33d1f2f73ec650117b8d
sx4im <msaimshafique4664@gmail.com>
2025-08-04 19:12:28 +0500

    solve problem 450

---
+++ Medium/450.js
@@ -0,0 +1,36 @@
+// 450. Delete Node in a BST
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function(root, key) {
+  if (!root) return root;
+  if (root.val < key) {
+    root.right = deleteNode(root.right, key);
+  } else if (root.val > key) {
+    root.left = deleteNode(root.left, key);
+  } else {
+    if (root.left === null) {
+      return root.right;
+    } else if (root.right === null) {
+      return root.left;
+    } else {
+      let node = root.right;
+      while (node.left) {
+        node = node.left;
+      }
+      node.left = root.left;
+      return root.right;
+    }
+  }
+  return root;
+};
