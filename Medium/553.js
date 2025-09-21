8a0c684434d217426e3c54fcff8a0ac5c75f4475
sx4im <msaimshafique4664@gmail.com>
2025-09-21 08:42:13 +0500

    solve 553

---
+++ Medium/553.js
@@ -0,0 +1,11 @@
+/**
+ * 553. Optimal Division
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var optimalDivision = function(nums) {
+    if (nums.length === 1) return nums[0].toString();
+    if (nums.length === 2) return `${nums[0]}/${nums[1]}`;
+    return `${nums[0]}/(${nums.slice(1).join('/')})`;
+  };
